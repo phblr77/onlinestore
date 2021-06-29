97d3fd0adc7d564ca0675b51305259e21cdb1b62
krukd <krukd87@gmail.com>
2021-06-29 12:48:22 +0200

    Initial commit

---
+++ src/test/java/ru/geekbrains/onlinestore/OnlinestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
